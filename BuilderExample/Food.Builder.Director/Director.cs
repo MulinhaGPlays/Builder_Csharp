2268cb88c2ec0290461b6b9a845c7f7a2f55c0fe
MulinhaGPlays <Android@labfies.org.br>
2022-10-27 14:37:19 -0300

    Implementação do builder

---
+++ BuilderExample/Food.Builder.Director/Director.cs
@@ -0,0 +1,19 @@
+﻿namespace Food.Builder.Director
+{
+    public class Director : IDirector
+    {
+        private MainBuilder builder = new MainBuilder();
+
+        public void setBuilder()
+        {
+            builder.Reset();
+        }
+
+        public void IniciateBuilder()
+        {
+            builder.MakePrincipal();
+            builder.MakeIntermediare();
+            builder.MakeFinale();
+        }
+    }
+}
---
+++ BuilderExample/Food.Builder.Director/Interfaces/IDirector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food.Builder.Director.Interfaces
+{
+    public interface IDirector
+    {
+        public void setBuilder();
+        public void IniciateBuilder();
+    }
+}
